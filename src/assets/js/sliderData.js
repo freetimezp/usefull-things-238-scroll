4de3d9953aa9d336d34538a066655d767cb0006c
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-08-02 21:28:00 +0300

    04. usefull-things-238-scroll. Data, Sample page.

---
+++ src/assets/js/sliderData.js
@@ -0,0 +1,42 @@
+export const sliderData = [
+    {
+        title: "Forgotten Truth",
+        img: "images/item-1.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Silent Serpent",
+        img: "images/item-2.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Snake's Legacy",
+        img: "images/item-3.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Weeping Flight",
+        img: "images/item-4.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Laughing Vision",
+        img: "images/item-5.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Moons of Soaring",
+        img: "images/item-6.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Roses's Emerald",
+        img: "images/item-7.jpg",
+        url: "/sample.html",
+    },
+    {
+        title: "Shores of the Edge",
+        img: "images/item-8.jpg",
+        url: "/sample.html",
+    },
+];
