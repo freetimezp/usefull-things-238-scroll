e948a497bbbf84343003269b2a9443a7beb0d5ef
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-11-08 17:52:25 +0200

    04. usefull-things-238-scroll. Fix.

--- src/assets/js/sliderData.js
+++ src/assets/js/sliderData.js
@@ -1,42 +1,42 @@
 export const sliderData = [
     {
         title: "Forgotten Truth",
-        img: "images/item-1.jpg",
+        img: "./src/assets/images/item-1.jpg",
         url: "/sample.html",
     },
     {
         title: "Silent Serpent",
-        img: "images/item-2.jpg",
+        img: "./src/assets/images/item-2.jpg",
         url: "/sample.html",
     },
     {
         title: "Snake's Legacy",
-        img: "images/item-3.jpg",
+        img: "./src/assets/images/item-3.jpg",
         url: "/sample.html",
     },
     {
         title: "Weeping Flight",
-        img: "images/item-4.jpg",
+        img: "./src/assets/images/item-4.jpg",
         url: "/sample.html",
     },
     {
         title: "Laughing Vision",
-        img: "images/item-5.jpg",
+        img: "./src/assets/images/item-5.jpg",
         url: "/sample.html",
     },
     {
         title: "Moons of Soaring",
-        img: "images/item-6.jpg",
+        img: "./src/assets/images/item-6.jpg",
         url: "/sample.html",
     },
     {
         title: "Roses's Emerald",
-        img: "images/item-7.jpg",
+        img: "./src/assets/images/item-7.jpg",
         url: "/sample.html",
     },
     {
         title: "Shores of the Edge",
-        img: "images/item-8.jpg",
+        img: "./src/assets/images/item-8.jpg",
         url: "/sample.html",
     },
 ];
